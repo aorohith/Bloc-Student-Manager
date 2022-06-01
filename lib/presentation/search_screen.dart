42e03ef883d3ac5e9a43616fa5ec3fb426ac124a
aorohith <aorohith@gmail.com>
2022-06-01 10:47:57 +0530

    Added onchange search result

--- lib/presentation/search_screen.dart
+++ lib/presentation/search_screen.dart
@@ -1,10 +1,14 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:student/infrastructure/student_model.dart';
+import 'package:student/presentation/student_details.dart';
 
 import '../application/student/student_cubit.dart';
+import '../core/constants.dart';
 
 class SearchScreen extends StatelessWidget {
   SearchScreen({Key? key}) : super(key: key);
@@ -13,42 +17,66 @@ class SearchScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     context.read<StudentCubit>().search("");
     return Scaffold(
-      body: SafeArea(
-        child: Column(
-          children: [
-            TextFormField(
-              controller: _search,
-            ),
-            ElevatedButton(
-              onPressed: () {
-                if (_search.text.trim().isEmpty) {
-                  return;
-                }
-                context.read<StudentCubit>().search(_search.text.trim());
-              },
-              child: Icon(
-                Icons.search,
-              ),
-            ),
-            BlocBuilder<StudentCubit, StudentState>(
-              builder: (context, state) {
-                return Expanded(
-                  child: ListView.builder(
-                    itemCount: state.searchResult.length,
+      appBar: AppBar(title: Text("Search Here")),
+      body: SafeArea(child: BlocBuilder<StudentCubit, StudentState>(
+        builder: (context, state) {
+          return Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: TextFormField(
+                    controller: _search,
+                    onChanged: (text) =>
+                        context.read<StudentCubit>().search(text),
+                    decoration: InputDecoration(
+                      hintText: "Enter Search Text",
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.separated(
                     itemBuilder: (context, index) {
-                      StudentModel student = state.searchResult[index];
-                      return ListTile(
-                        title: Text(student.name),
-                        subtitle: Text(student.division),
+                      // state as StudentInitial;
+                      StudentModel student = state.students[index];
+                      print(student.photo);
+                      return Card(
+                        child: ListTile(
+                          leading: student.photo == null
+                              ? CircleAvatar(
+                                  backgroundImage: NetworkImage(
+                                      'https://e7.pngegg.com/pngimages/78/788/png-clipart-computer-icons-avatar-business-computer-software-user-avatar-child-face.png'),
+                                  radius: 50,
+                                )
+                              : CircleAvatar(
+                                  backgroundImage:
+                                      FileImage(File(student.photo.toString())),
+                                  radius: 50,
+                                ),
+                          title: Text(student.name),
+                          subtitle: Text(student.division),
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => StudentDetailPage(
+                                          index: index,
+                                        )));
+                          },
+                        ),
                       );
                     },
+                    separatorBuilder: (context, index) => kHeight,
+                    itemCount: state.students.length,
                   ),
-                );
-              },
-            )
-          ],
-        ),
-      ),
+                )
+              ],
+            ),
+          );
+        },
+      )),
     );
   }
 }
